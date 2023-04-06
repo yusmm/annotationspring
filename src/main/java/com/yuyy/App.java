62192b7db80cea45c66c48f398deee1a1bde1def
yusmm <1186490218@qq.com>
2023-04-06 17:24:03 +0800

    Initial commit

---
+++ src/main/java/com/yuyy/App.java
@@ -0,0 +1,13 @@
+package com.yuyy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
